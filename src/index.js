c813991fa6984b7ad27535c3e8f199ed6c81429a
Mirah Eka <miraheka@outlook.com>
2019-08-15 16:48:12 +0700

    redux example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import appReducer from "./reducers";
+import App from "./App";
+import Results from "./components/Results";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let storeData = createStore(appReducer);
+
+function appRender() {
+  ReactDOM.render(
+    <div className="container">
+      <App store={storeData} />
+      <hr />
+      <Results store={storeData} />
+    </div>,
+    document.getElementById("root")
+  );
+}
+
+storeData.subscribe(appRender);
+appRender();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
